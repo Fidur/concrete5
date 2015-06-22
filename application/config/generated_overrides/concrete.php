7a6bf83edaf578a30d58a61ecc34984240814a0a
Łukasz Fidurski <fidurski.l@gmail.com>
2015-06-23 01:29:23 +0200

    gogogo

---
+++ application/config/generated_overrides/concrete.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-05-12T13:12:54+02:00
+ *
+ * @item      misc.latest_version
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'site' => 'Łukasz Fidurski',
+    'version_installed' => '5.7.3.1',
+    'misc' => array(
+        'access_entity_updated' => 1430826820,
+        'latest_version' => '5.7.3.1',
+        'seen_introduction' => true,
+        'do_page_reindex_check' => false
+    )
+);
---
+++ application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/82b9566cc1f9a7dc/15efdc59feb96781.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_287273492c873b323bdb1d292da032b3_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1431554092;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1431166227;
